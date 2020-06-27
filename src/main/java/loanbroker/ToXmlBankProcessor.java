5510cf33e7bbc2a2e38fe628663ea30b61e080da
OfenPower <danielbach56@gmail.com>
2020-06-27 18:45:30 +0200

    ToXmlBankProcessor

---
+++ src/main/java/loanbroker/ToXmlBankProcessor.java
@@ -0,0 +1,30 @@
+package loanbroker;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class ToXmlBankProcessor implements Processor {
+
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		
+		// Json Message auslesen
+		String jsonString = exchange.getIn().getBody(String.class);
+		
+		// XML Mapper zum translaten verwenden
+		ObjectMapper objectMapper = new XmlMapper();
+		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+		String xml = objectMapper.writeValueAsString(jsonString);
+		
+		System.out.println(xml);
+		
+		// XML Message rausschicken
+		exchange.getIn().setBody(xml);
+		
+	}
+
+}
