a66b199e004053d9240d4a8b7b03cb79efc915de
david <davidsiepen95@gmail.com>
2020-06-30 17:44:57 +0200

    ClearText Normalizer

--- src/main/java/loanbroker/Normalizer.java
+++ src/main/java/loanbroker/Normalizer.java
@@ -1,10 +1,13 @@
 package loanbroker;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.camel.Exchange;
 
 public class Normalizer {
 	public void normalizeJson(Exchange exchange) {
-		System.out.println("normalizeJson");
 	}
 	
 	public void normalizeXml(Exchange exchange) {
@@ -12,6 +15,22 @@ public class Normalizer {
 	}
 	
 	public void normalizeClearText(Exchange exchange) {
-		System.out.println("normalizeClearText");
+
+		String msg = exchange.getIn().getBody(String.class);
+		String fields[] = msg.split(",");
+
+		double requestedFunds = Double.parseDouble(fields[0].split(" ")[1]);
+		double startCapital = Double.parseDouble(fields[1].split(" ")[1]);
+		double monthlyIncome = Double.parseDouble(fields[2].split(" ")[1]);
+		int creditScore = Integer.parseInt(fields[3].split(" ")[1]);
+
+		ObjectMapper mapper = new ObjectMapper();
+		ObjectNode node = mapper.createObjectNode();
+		node.put("requestedFunds",requestedFunds);
+		node.put("startCapital", startCapital);
+		node.put("monthlyIncome",monthlyIncome);
+		node.put("creditScore",creditScore);
+		exchange.getIn().setBody(node.toString());
+		System.out.println("Normalized ClearTextResponse to: " + node.toString());
 	}
 }
