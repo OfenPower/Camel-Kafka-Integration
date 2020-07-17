c526b0b996a981e2f34962d8247c1a85c80fe35d
Glumbatsch <davidsiepen95@gmail.com>
2020-07-17 14:01:40 +0200

    Aggregation Strategy kommentiert.

--- src/main/java/loanbroker/BankResponseAggregationStrategy.java
+++ src/main/java/loanbroker/BankResponseAggregationStrategy.java
@@ -11,45 +11,54 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class BankResponseAggregationStrategy implements AggregationStrategy, Predicate {
 
-	/**
-	 * @param oldExchange aggregation so far
-	 * @param newExchange new incoming message
-	 */
+	/*
+	oldExchange: Bisher Aggregierte Nachricht.
+	newExchange: Neu empfangene Nachricht.
+	return: Aggregation in diesem Schritt (im nächsten schritt ist die Rückgabe der oldExchange).
+	*/
 	public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
 
 
 		// aggregated message:
 		/*
 		{
-			Bank Teil: das beste angebot nehmen
-        	"monthlyPremiums" : 123.4,
-        	"durationInMonths" : 1,
-        	"grantedCredit" : 123.4,
-        	"interestRatePerMonth" : 123.4,
-			Aggregation Teil:
+		Bank Teil: das beste angebot nehmen
+        		"monthlyPremiums" : 123.4,
+        		"durationInMonths" : 1,
+        		"grantedCredit" : 123.4,
+        		"interestRatePerMonth" : 123.4,
+		Aggregation Teil:
 			"expectedResponseCount" : 1,
 			"receivedResponseCount" : 1,
 		}
 		 */
 		ObjectMapper mapper = new ObjectMapper();
 		try {
+			// eingehende Nachricht parsen.
 			JsonNode newNode = mapper.readTree(newExchange.getIn().getBody(String.class));
 			ObjectNode newObj = (ObjectNode)newNode;
 
+			// Falls oldExchange null ist, ist newExchange die erste Nachricht ihrer Gruppe (d.h. es wurde in der Gruppe noch nichts aggregiert).
 			if (oldExchange == null)
 			{
+				// Nachricht um Aggregations-Informationen erweitern und zurückgeben
+				// Im nächsten Schritt ist diese Nachricht der oldExchange.
 				newObj.put("expectedResponseCount",ExpectedResponseDatabase.getInstance().map.get(newNode.get("correlationId").asInt()));
 				newObj.put("receivedResponseCount",1);
 				newExchange.getIn().setBody(newObj.toString());
 				return newExchange;
 			}
-
+			
+			// bisher aggregierte Nachricht parsen.
 			JsonNode oldNode = mapper.readTree(oldExchange.getIn().getBody(String.class));
 			ObjectNode oldObj = (ObjectNode)oldNode;
-
+			
+			// Zinssätze auslesen
 			double oldInterest = oldObj.get("interestRatePerMonth").asDouble();
 			double newInterest = newObj.get("interestRatePerMonth").asDouble();
 
+			// Zinssätze vergleichen und das Angebot mit dem besseren Zinssatz übernehmen.
+			// Zusätzlich werden die Aggregations-Informationen aktualisiert.
 			if(oldInterest <= newInterest){
 				oldObj.put("receivedResponseCount",oldObj.get("receivedResponseCount").asInt() +1);
 				oldObj.put("expectedResponseCount",ExpectedResponseDatabase.getInstance().map.get(newNode.get("correlationId").asInt()));
@@ -66,6 +75,9 @@ public class BankResponseAggregationStrategy implements AggregationStrategy, Pre
 		return null;
 	}
 
+	// Dies Funktion  implementiert die Completeness Condition
+	// Falls die Funktion true zurückgibt gilt die Gruppe als vollständig.
+	// Funktion wird nach aggregate(...) aufgerufen.
 	@Override
 	public boolean matches(Exchange exchange) {
 		ObjectMapper mapper = new ObjectMapper();
@@ -73,6 +85,7 @@ public class BankResponseAggregationStrategy implements AggregationStrategy, Pre
 			JsonNode node = mapper.readTree(exchange.getIn().getBody(String.class));
 			int expected = node.get("expectedResponseCount").asInt();
 			int received = node.get("receivedResponseCount").asInt();
+			// Falls wir so viele Nachrichten aggregiert haben, wie wir in dieser Gruppe erwarten sind wir fertig.
 			return expected == received;
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
